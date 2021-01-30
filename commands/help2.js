faa0d427a32a04a94af4c9d690959930037d468e
Glitch (available-heather-linseed) <none>
2021-01-30 03:18:25 +0000

    🎰👹 Checkpoint ./commands/help2.js:22420122/2666

--- commands/help2.js
+++ commands/help2.js
@@ -1,42 +1,30 @@
-const { MessageEmbed } = require("discord.js");
+const Discord = require('discord.js')
 
-/*module.exports = {
-  name: "help",
-  aliases: ["hp"],
-  description: "Shows You This Message!",
-  execute(message) {  */
-    module.exports.run = async (bot, message, args) => {
-    let commands = message.client.commands.array();
 
-    let helpEmbed = new MessageEmbed()
-      .setTitle(`${message.client.user.username} Help`)
-      .setDescription("__**Take a look at the commands i have to play music**__")
-      .setColor("#FF0000")
-      .setTimestamp()
-      .setThumbnail(
-        "https://cdn.discordapp.com/attachments/793513783175413763/798864136195801108/Screenshot_20210113-161015_One_UI_Home.png"
-      );
-    commands.forEach(cmd => {
-      helpEmbed.addField(
-        `<:music:798941618136809482> **Az!${cmd.name} ${
-          cmd.aliases ? `(${cmd.aliases})` : ""
-        }**`,
-        `${cmd.description}\n`,
-        true
-      );
-    });
-   // helpEmbed.addField(
-     // "For support join our server!",
-      //"[click here to join our support server](https://discord.gg/kzcwumBhHa)"
-   // );
+module.exports.run = async (bot, message, args) => {
+  
+  let prefix = process.env.prefix
+  
+  const embed = new Discord.MessageEmbed()
 
-    helpEmbed.setTimestamp();
+let command = bot.commands.get(bot.aliases.get)//(args[0].toLowerCase()) || args[0].toLowerCase())
+            if (!command) return message.channel.send(embed.setTitle("**Invalid Command!**").setDescription(`**Do \`${prefix}help\` For the List Of the Commands!**`))
+            command = command.config
 
-    return message.channel.send(helpEmbed).catch(console.error);
-  }
-    
-  module.exports.help = {
+            embed.setColor('GOLD')  
+            embed.setDescription(`**${bot.user.username}'s' Prefix Is \`${prefix}\`**\n
+            ** Command -** ${command.name.slice(0, 1).toUpperCase() + command.name.slice(1)}\n
+            ** Description -** ${command.description || "No Description provided."}\n
+            ** Usage -** ${command.usage ? `\`${prefix}${command.name} ${command.usage}\`` : "No Usage"}\n
+            ** Needed Permissions -** ${command.accessableby || "everyone can use this command!"}\n
+            ** Aliases -** ${command.aliases ? command.aliases.join(", ") : "None."}`)
+            embed.setFooter(message.guild.name, message.guild.iconURL())
+
+            return message.channel.send(embed)
+  
+}
+
+module.exports.help = {
   name:"help2",
   aliases: ["h2"]
 }
-
