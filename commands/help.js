1e0bb73e29c82fb55cf25a88628649e12046a6aa
Glitch (available-heather-linseed) <none>
2021-02-01 15:57:26 +0000

    👴🍡 Checkpoint ./commands/help.js:22420122/504

--- commands/help.js
+++ commands/help.js
@@ -7,10 +7,15 @@ module.exports.run = async (bot, message, args) => {
 
     let embed = new Discord.MessageEmbed()
     .setTitle("**__LIST OF MY COMMANDS__**")
+    //.addField("•────────────────────────────•","")
+    //.addField("My Prefix is **Az!**","")
+    //.addField("•────────────────────────────•","")
+    .setDescription(`•────────────────────────────•\nMy Prefix is **Az!**\n•────────────────────────────•`)
     .addField("> <:pikasip:803606908103032852> **1. General Commands**","`Ping`,`info <user>`,`afk`,`back`,`avatar <user>`,`embed`")
     .addField("> <a:server:805812169521889340> **2. Server Commands**", "`staff`, `rules for staff`,`order`,`apply`,`vote`")//itna fast kaisai ;-;
-    .addField("> <a:moneyaabag:802503275085299752> **3. Economy Commands**", "`work`,`beg`,`rob`,`pay`,`balance`,`profile`,`withdraw`,`deposit`,`daily`\n`weekly`,`store`,`buy` ")
+    .addField("> <a:moneyaabag:802503275085299752> **3. Economy Commands**", "`work`,`beg`,`rob`,`pay`,`balance`,`profile`,`withdraw`,`deposit`,`daily`,`weekly`,`store`,`buy` ")
     .addField("> <a:paisa:805769554470633482> **4. Gambling Commmands**", "`slots`")
+    .setImage("https://cdn.discordapp.com/attachments/735352832701759560/737667554406825994/ars.gif")
     //.addField("> Economy Extra Commands", "`store`")
     .setColor("GOLD")
     .setThumbnail("https://cdn.discordapp.com/avatars/804254260308082719/4f345b974bc5643d4886bfb4a67f2ed0.png?size=1024")
