878d41e136ffc66e4cb30e120c6d383fc9f9b7f5
Glitch (available-heather-linseed) <none>
2021-02-01 06:07:29 +0000

    🙍🖼 Checkpoint ./commands/profile.js:22420122/1597 ./commands/store.js:22420122/2

--- commands/profile.js
+++ commands/profile.js
@@ -16,25 +16,49 @@ module.exports.run = async (bot, message, args) => {
     if(vip === null) vip = 'None'
     if(vip === true) vip = '<@&804991402525327420>'
 
+  let giratina = await db.fetch(`giratina_${message.guild.id}_${user.id}`)
+  if(giratina === null) giratina = '<:arzeus_cross:804612025379586058>'
+  
+  let palkia = await db.fetch(`palkia_${message.guild.id}_${user.id}`)
+  if(palkia === null) palkia = '<:arzeus_cross:804612025379586058>'
+  
+  let dialga = await db.fetch(`dialga_${message.guild.id}_${user.id}`)
+  if(dialga === null) shoes = '<:arzeus_cross:804612025379586058>'
+  
   let shoes = await db.fetch(`shoes_${message.guild.id}_${user.id}`)
   if(shoes === null) shoes = '0'
   
   let tshirt = await db.fetch(`tshirt_${message.guild.id}_${user.id}`)
   if(tshirt === null) tshirt = '0'
+  
+  let designersuit = await db.fetch(`designersuit_${message.guild.id}_${user.id}`)
+  if(designersuit === null) designersuit = '0'
 
   let car = await db.fetch(`car_${message.guild.id}_${user.id}`)
   if(car === null) car = '0'
 
   let house = await db.fetch(`house_${message.guild.id}_${user.id}`)
   if(house === null) house = '0'
+  
+  let hamster = await db.fetch(`hamster_${message.guild.id}_${user.id}`)
+  if(hamster === null) hamster = '0'
+  
+  let fish = await db.fetch(`fish_${message.guild.id}_${user.id}`)
+  if(fish === null) fish = '0'
+  
+  let cat = await db.fetch(`cat_${message.guild.id}_${user.id}`)
+  if(cat === null) cat = '0'
+  
+  let dog = await db.fetch(`dog_${message.guild.id}_${user.id}`)
+  if(dog === null) dog = '0'
 
   let moneyEmbed = new Discord.MessageEmbed()
   .setColor("GOLD")
-  .setDescription(`**${user}'s Profile**\n\nWallet: ${money} <:arzeus_coin:804370629654347788>\nBank: ${bank} <:arzeus_coin:804370629654347788>\nVIP Roles: ${vip}\n\n**Inventory**\n\nShoes: ${shoes}\nCars: ${car}\nHouses: ${house}\nT-Shirts: ${tshirt}`);
+  .setDescription(`**${user}'s Profile**\n\nWallet: ${money} <:arzeus_coin:804370629654347788>\nBank: ${bank} <:arzeus_coin:804370629654347788>\n**VIP Roles**\n\nGiratina: ${giratina}\nPalkia: ${palkia}\nDialga: ${dialga}\n\n**Inventory**\n\nShoes: ${shoes}\nCars: ${car}\nHouses: ${house}\nT-Shirts: ${tshirt}\nSuits: ${designersuit}\n\n**PETS**\n\nHamsters: ${hamster}\nFishes: ${fish}\nCats: ${cat}\nDogs: ${dog}`);
   message.channel.send(moneyEmbed)
 };
 
 module.exports.help = {
   name:"profile",
-  aliases: ["pro"]
+  aliases: ["pf"]
 }
