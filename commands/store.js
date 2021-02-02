d15b7d1c6c9ffec93599541da9af916ac932762e
Glitch (available-heather-linseed) <none>
2021-02-02 11:45:58 +0000

    🚃📮 Checkpoint ./commands/profile.js:22420122/501 ./commands/store.js:22420122/323

--- commands/store.js
+++ commands/store.js
@@ -10,6 +10,7 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("**__ITEMS IN SHOP__**")
     .setDescription("**VIP Roles**\n\nGIRATINA: 30K <:arzeus_coin:804370629654347788>\nPALKIA: 40K <:arzeus_coin:804370629654347788>\nDIALGA: 50K <:arzeus_coin:804370629654347788>\n\n**LIFESTYLES**\n\nSHOES: 1K <:arzeus_coin:804370629654347788>\nT-SHIRT: 3K <:arzeus_coin:804370629654347788>\nDESIGNER_SUIT: 6K <:arzeus_coin:804370629654347788>\nHOUSE: 40K <:arzeus_coin:804370629654347788>\nCAR: 20K <:arzeus_coin:804370629654347788>\n\n**PETS**\n\nHAMSTER: 2K <:arzeus_coin:804370629654347788>\nFISH: 4K <:arzeus_coin:804370629654347788>\nCAT: 6K <:arzeus_coin:804370629654347788>\nDOG: 10K <:arzeus_coin:804370629654347788>\n\n**SERVER SHOP**\n\nSNIPE PASS(1 WEEK): 90K  <:arzeus_coin:804370629654347788>\nSPAM PASS(1 WEEK): 80K <:arzeus_coin:804370629654347788>")
     .setColor("GOLD")
+   .setImage("https://cdn.discordapp.com/attachments/735352832701759560/737667554406825994/ars.gif")
     .setThumbnail("https://cdn.discordapp.com/avatars/804254260308082719/4f345b974bc5643d4886bfb4a67f2ed0.png?size=1024")
     message.channel.send(embed)
 
