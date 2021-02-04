46ef337890eeccb3be3710b97344d1877f3f6620
Glitch (available-heather-linseed) <none>
2021-02-04 15:21:05 +0000

    🏟💺 Checkpoint ./commands/pay5.js:22420122/2183

---
+++ commands/pay5.js
@@ -0,0 +1,68 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+const ms = require("parse-ms");
+
+module.exports.run = async (bot, message, args) => {
+ // if(!message.content.startsWith('e!'))return;  
+
+  let user = message.mentions.members.first() 
+
+  let member = db.fetch(`money_${message.guild.id}_${message.author.id}`)
+
+  let embed1 = new Discord.MessageEmbed()
+  .setColor("GOLD")
+  .setDescription(`<a:cross:805816169973809203>| Mention someone to pay`);
+
+  if (!user) {
+      return message.channel.send(embed1)
+  }
+  let embed2 = new Discord.MessageEmbed()
+  .setColor("GOLD")
+  .setDescription(`<a:cross:805816169973809203>| Specify an amount to pay`);
+  
+  if (!args[1]) {
+      return message.channel.send(embed2)
+  }
+  let embed3 = new Discord.MessageEmbed()
+  .setColor("GOLD")
+  .setDescription(`<a:cross:805816169973809203>| You can't pay someone negative money`);
+
+  if (message.content.includes('-')) { 
+      return message.channel.send(embed3)
+  }
+  let embed4 = new Discord.MessageEmbed()
+  .setColor("GOLD")
+  .setDescription(`<a:cross:805816169973809203>| You don't have that much money`);
+
+  if (member < args[1]) {
+      return message.channel.send(embed4)
+  }
+  
+  if (args[1] == 'all') {
+    let money = await db.fetch(`money_${message.guild.id}_${user.id}`)
+   // let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`)
+
+    let embedbank = new Discord.MessageEmbed()
+    .setColor('GOLD')
+    .setDescription("<a:cross:805816169973809203>| You don't have any money to Pay")
+
+    if(money === 0) return message.channel.send(embedbank)
+
+    db.add(`money_${message.guild.id}_${user.id}`, money)
+    db.subtract(`money_${message.guild.id}_${message.author.id}`, money)
+  }
+
+  let embed5 = new Discord.MessageEmbed()
+  .setColor("GOLD")
+  .setDescription(`<a:tick:805814130304483358>| You have payed ${user.user.username} ${args[1]} <:arzeus_coin:804370629654347788>`);
+
+  message.channel.send(embed5)
+  db.add(`money_${message.guild.id}_${user.id}`, args[1])
+  db.subtract(`money_${message.guild.id}_${message.author.id}`, args[1])
+
+}
+
+module.exports.help = {
+  name:"pay5",
+  aliases: [""]
+}
